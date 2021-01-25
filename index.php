5243d253420e0debab12d3fa5fe59bf35246c39b
Eunbyul na Proctor <51739602+EunbyulNa@users.noreply.github.com>
2021-01-25 13:21:21 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -44,16 +44,23 @@ function print_list(){
       ?>
     </ol>
     <a href="create.php">create</a>
-    <form action="create_process.php" method="post">
-      <p>
-        <input type="text" name="title" placeholder="Title">
-      </p>
-      <p>
-        <textarea name="description" placeholder="Description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
+    <?php if(isset($_GET['id'])){ ?>
+    <a href="update.php?id=<?=$_GET['id']?>">update</a>
+
+    <form action="delete_process.php" method="post">
+      <input type="hidden" name="id" value="<?=$_GET['id']?>">
+      <input type="submit" name="" value="Delete">
     </form>
+   <?php } ?>
+
+
+    <h2>
+      <?php
+      print_title();
+      ?>
+    </h2>
+    <?php
+    print_description();
+     ?>
   </body>
 </html>
